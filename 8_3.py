2c4ddd382885b262fdefc98d984e3718f9bdad0e
liukuai <937613925@qq.com>
2019-09-09 17:29:46 +0800

    init python_practice

---
+++ 8_3.py
@@ -0,0 +1,4 @@
+def make_shirt(size, text):
+    print('You need a shirt with size' + " " + "'" + size + "'" + " " + 'and with word' + " " + "'" + text + "'" + " " + 'on it!' )
+
+make_shirt('xxl', 'Vue')
