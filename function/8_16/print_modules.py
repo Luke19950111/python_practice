aec65677b5225bd4ecdef1a2591c79c238a65cfa
liukuai <937613925@qq.com>
2019-09-10 11:32:34 +0800

    函数-将函数存储在模块中

---
+++ function/8_16/print_modules.py
@@ -0,0 +1,14 @@
+# import print_function
+# print_function.print_something('got it!')
+
+# from print_function import  print_something
+# print_something('got it again!')
+
+# from  print_function import  print_something as p
+# p('gg')
+
+# import print_function as p
+# p.print_something('aa')
+
+from print_function import *
+print_something('all in')
